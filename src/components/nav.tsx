c5404d01a96a197c316916f62a8636bcdfb1f229
newtoallofthis123 <noobscience123@gmail.com>
2024-05-01 13:00:38 +0530

    Trim in home page

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,7 +8,7 @@ export default function Nav({ title, emoji }: Props) {
     <nav className="flex md:flex-row flex-col justify-between items-center p-2 md:border-2 border-b-2 border-dark dark:border-neutral-200 md:my-4">
       <div className="md:my-0 mb-3">
         <a href="/" className="p-1 text-xl bg-rosePine-pine font-bold">
-          <span className="px-3">{emoji ?? "@"}</span> {title ?? "NoobScience"}
+          <span className="px-3">{emoji ?? "*"}</span> {title ?? "NoobScience"}
         </a>
       </div>
       <div>
