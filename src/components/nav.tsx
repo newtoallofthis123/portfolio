e7978eb416387ac55e0baec60a8b2b8006f83fb1
newtoallofthis123 <noobscience123@gmail.com>
2024-05-01 16:37:52 +0530

    Improvements

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,14 +1,22 @@
 type Props = {
   title?: string;
   emoji?: string;
+  color?: string;
 };
 
-export default function Nav({ title, emoji }: Props) {
+export default function Nav({ title, emoji, color }: Props) {
+  let options = ["gold", "foam"];
+  let ranChoice = options[Math.floor(Math.random() * options.length)];
+
   return (
     <nav className="flex md:flex-row flex-col justify-between items-center p-2 md:border-2 border-b-2 border-dark dark:border-neutral-200 md:my-4">
       <div className="md:my-0 mb-3">
-        <a href="/" className="p-1 text-xl bg-rosePine-pine font-bold">
-          <span className="px-3">{emoji ?? "*"}</span> {title ?? "NoobScience"}
+        <a
+          href="/"
+          className={`p-1 text-xl bg-rosePine-${ranChoice} text-rosePine-overlay font-bold`}
+        >
+          <span className="px-3">{emoji ?? "*"}</span>
+          {title ?? "NoobScience"}
         </a>
       </div>
       <div>
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,31 @@
 import { z, defineCollection } from 'astro:content';
 
-// So I know I have a option problem.
-// But I have no idea how this grew so much.
-// Now I am too scared to remove any of these.
-// Is this how you feel when you have a lot of options?
-// I guess I will never know.
 const blogContent = defineCollection({
     type: 'content',
-    schema: ({image}) => z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         author: z.string().default('Ishan Joshi'),
         date: z.date(),
-        description: z.string(),
+        description: z.string().default(''),
         emoji: z.string().optional(),
-        tags: z.array(z.string()),
+        color: z.string().default('pine'),
+        tags: z.array(z.string()).optional(),
         draft: z.boolean().default(false),
         img: image().refine((img) => img).optional(),
     }),
 });
 
+const thinkingContent = defineCollection({
+    type: 'content',
+    schema: () => z.object({
+        title: z.string(),
+        author: z.string().default('Ishan Joshi'),
+        color: z.string().default('pine'),
+        date: z.date().default(new Date()),
+    }),
+});
+
 export const collections = {
     blog: blogContent,
+    thinks: thinkingContent,
 };
