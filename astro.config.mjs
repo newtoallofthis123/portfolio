a4864b779536b1b78bcc976c5aaf0d49894aacc9
newtoallofthis123 <noobscience123@gmail.com>
2024-05-29 00:13:35 +0530

    Add SEO Optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig, squooshImageService } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
+    site: 'https://www.noobscience.in',
     adapter: vercel({
         webAnalytics: { enabled: true }
     }),
-    integrations: [react(), tailwind(), mdx()],
+    integrations: [react(), tailwind(), mdx(), sitemap()],
     image: {
         service: squooshImageService(),
     },
